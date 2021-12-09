92e6c8722fd2aea8a9f41662010bc3c62582be87
Cody Leedom <Cody@uruktek.com>
2021-12-09 15:16:46 -0600

    [Test] Troubleshooting error message about iex

--- Testing/Test-VersionOfInstaller.ps1
+++ Testing/Test-VersionOfInstaller.ps1
@@ -1,9 +1,13 @@
 #THIS IS THE TEST VERSION WHICH MAY OR MAY NOT BE BROKEN
 #USE AT YOUR OWN RISK
 #This installs nuget so that the install-mod will not prompt for it
+Write-host "Installing NuGet...."
 Install-PackageProvider NuGet -Force > $null
 #this installs pswindowsupdate to install windows update from ps
+Write-Host "Installing pswindowsupdate....."
 Install-Module pswindowsupdate -Confirm:$false -Force > $null
+Clear-Host
+
 $ProgramsToInstall = Get-Content ..\apps.txt
 try {
     choco -v > $null
@@ -19,14 +23,14 @@ try {
 catch [System.Management.Automation.CommandNotFoundException]{
     Write-host "Choco package manager is not installed."
     Write-host "Choco will now be installed...."
-    set-ExecutionPolicy Bypass -Scope Process -Force > $null #execution before running should be changed however this is suggested by the dev. Hush output.
+    #set-ExecutionPolicy Bypass -Scope Process -Force > $null #execution before running should be changed however this is suggested by the dev. Hush output.
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     Clear-Host
     Write-Host "Install apps from the app file......."
     foreach ($app in $ProgramsToInstall) {
-        choco install $app -r -y
-        $Host.ui.RawUI.WindowTitle = "Installing " + $app + "...."
         Clear-Host
+        $Host.ui.RawUI.WindowTitle = "Installing " + $app + "...."
+        choco install $app -r -y
     }
 
     Clear-Host

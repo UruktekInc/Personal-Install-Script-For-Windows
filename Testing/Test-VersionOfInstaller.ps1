3872114e45dc4816c68a0b66ddc7de792372fff9
Cody Leedom <Cody@uruktek.com>
2021-12-09 12:58:09 -0600

    [test] added app.txt to pull files

--- Testing/Test-VersionOfInstaller.ps1
+++ Testing/Test-VersionOfInstaller.ps1
@@ -8,27 +8,26 @@ Install-PackageProvider NuGet -Force
 #this installs pswindowsupdate to install windows update from ps
 Install-Module pswindowsupdate -Confirm:$false -Force
 
-
 #TODO: Use gc to pull from app.txt of the list of file to install rather than using a hard coded arraylist
 #$ProgramsToInstall = "steam","GoogleChrome","FireFox","origin","vscode","vlc","powertoys","microsoft-windows-terminal","ccleaner","7zip","filezilla","git","github-desktop"
-$ProgramsToInstall = Get-Content .\apps.txt
+$ProgramsToInstall = Get-Content ..\apps.txt
 
 if (-not ($isChocoInstalled)) {
     Write-host "Choco package manager is not installed or can not be found"
     set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     foreach ($app in $ProgramsToInstall) {
+        #TODO: Add supressed output to screen EG "installing X..."
         choco install $app -y
     }
 }
 else {
     Write-host "Choco is installed." $isChocoInstalled
     foreach ($app in $ProgramsToInstall) {
+        #TODO: Add supressed output to screen EG "installing X..."
         choco install $app -y
     }
 }
 
-#TODO:FOR TESTING REASONS WINDOWS UPDATES ARE DISABLED BY DEFAULT
-
-# #Installing windows update from pswindowsupdate module
-# Write-Host "Everything within your list is installed. Now running windows updates"
-# Install-WindowsUpdate -Confirm:$false
+#Installing windows update from pswindowsupdate module
+Write-Host "Everything within your list is installed. Now running windows updates"
+Install-WindowsUpdate -Confirm:$false

6a6fb8ad42830ac8fed34cdb613774d26830aa2d
Cody Leedom <Cody@uruktek.com>
2021-12-09 10:40:48 -0600

    Fixing code outside of loop

--- get-personalInstaller.ps1
+++ get-personalInstaller.ps1
@@ -2,20 +2,18 @@
 
 $isChocoInstalled = powershell choco -v
 
+$ProgramsToInstall = "steam","GoogleChrome","FireFox","origin","vscode","vlc","powertoys","microsoft-windows-terminal","ccleaner","7zip","filezilla","git","github-desktop","battle.net"
+
 if (-not ($isChocoInstalled)) {
     Write-host "Choco package manager is not installed or can not be found"
     set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    foreach ($app in $ProgramsToInstall) {
+        choco install $app -y
+    }
 }
 else {
     Write-host "Choco is installed." $isChocoInstalled
-}
-#to install without prompt pass -y or enable it globally
-
-#TODO: above is commented for the time being during testing please remove all the comments from the above code.
-
-$ProgramsToInstall = "steam","GoogleChrome","FireFox","origin","vscode","vlc","powertoys","microsoft-windows-terminal","ccleaner","7zip","filezilla","git","github-desktop","battle.net"
-
-
-foreach ($app in $ProgramsToInstall) {
-    choco install $app -y
+    foreach ($app in $ProgramsToInstall) {
+        choco install $app -y
+    }
 }

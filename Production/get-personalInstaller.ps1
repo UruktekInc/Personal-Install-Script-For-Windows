4e735ce6a48eda01677db8ba7a4026534e107858
Cody Leedom <Cody@uruktek.com>
2021-12-09 13:44:36 -0600

    merge prod and testing

--- Production/get-personalInstaller.ps1
+++ Production/get-personalInstaller.ps1
@@ -1,36 +1,35 @@
+#THIS IS THE TEST VERSION WHICH MAY OR MAY NOT BE BROKEN
+#USE AT YOUR OWN RISK
+
 #Checking for choco
-$isChocoInstalled = choco -v
-
-#Known issues
-#TODO: Battle.net prompts that the hash is mismatched and prompts for further confirmation you can press y
-#TODO: Also bnet prompts for further install instructions may want to remove this for the time being
-#TODO: Github desktop will auto launch
-
-#install windows update ps module
-
+$isChocoInstalled = choco -v 
 #This installs nuget so that the install-mod will not prompt for it
-Install-PackageProvider NuGet -Force
+Install-PackageProvider NuGet -Force > $null
 #this installs pswindowsupdate to install windows update from ps
-Install-Module pswindowsupdate -Confirm:$false -Force
+Install-Module pswindowsupdate -Confirm:$false -Force > $null
 
-
-#Perhaps this should be inside an txt file or an ini for simplicity?
-$ProgramsToInstall = "steam","GoogleChrome","FireFox","origin","vscode","vlc","powertoys","microsoft-windows-terminal","ccleaner","7zip","filezilla","git","github-desktop"
+#TODO: Use gc to pull from app.txt of the list of file to install rather than using a hard coded arraylist
+#$ProgramsToInstall = "steam","GoogleChrome","FireFox","origin","vscode","vlc","powertoys","microsoft-windows-terminal","ccleaner","7zip","filezilla","git","github-desktop"
+$ProgramsToInstall = Get-Content ..\apps.txt
 
 if (-not ($isChocoInstalled)) {
     Write-host "Choco package manager is not installed or can not be found"
     set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
     foreach ($app in $ProgramsToInstall) {
-        choco install $app -y
+        #TODO: Add supressed output to screen EG "installing X..."
+        choco install $app -r -y
     }
 }
 else {
     Write-host "Choco is installed." $isChocoInstalled
     foreach ($app in $ProgramsToInstall) {
+        #TODO: Add supressed output to screen EG "installing X..."
         choco install $app -y
     }
 }
 
+
 #Installing windows update from pswindowsupdate module
 Write-Host "Everything within your list is installed. Now running windows updates"
-Install-WindowsUpdate -Confirm:$false
+Write-host "Please wait as this may take some time...."
+Install-WindowsUpdate -Confirm:$false > $null
